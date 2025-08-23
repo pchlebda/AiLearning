475684b824ea460bfb1a829e7ab9804cf2b33dbd
piotr <piotr.chlebda.ext@iceservices.com>
2025-08-23 21:02:09 +0200

    Feature engineering

---
+++ learning/feautre_engineering.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+
+data = {'Color': ['Red', 'Blue', 'Green', 'Blue']}
+df = pd.DataFrame(data)
+
+df_encoded = pd.get_dummies(df, columns=['Color'], prefix='Color')
+
+print(df_encoded)
+
+data = {'Age': [23, 45, 18, 34, 67, 50, 21]}
+
+df = pd.DataFrame(data)
+
+bins = [0, 20, 40, 60, 100]
+
+labels = ['0-20', '21-40', '41-60', '61+']
+
+df['Age_Group'] = pd.cut(df['Age'], bins=bins, labels=labels, right=False)
+
+print(df['Age_Group'])
